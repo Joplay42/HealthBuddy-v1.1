3223627c28b34a4cac6b2a9bfb7112853ee7d168
Joplay42 <jojobanjo2611@gmail.com>
2025-03-06 18:06:25 -0500

    Fixed bug with no objective

--- components/dashboard/home/Calories.tsx
+++ components/dashboard/home/Calories.tsx
@@ -1,19 +1,18 @@
 "use client";
 // Components import
 import { DisplayCalories } from "@/components";
+import { useUserInformationContext } from "@/context/UserInformationContext";
 import { useRouter } from "next/navigation";
 
 /**
  * This component is used to display the content in the calories page. It assembles many
  * component to make the view stable and easy to use.
- *
- * @param goal user goal
- * @param data user data
  * @returns
  */
 const Calories = () => {
   // Router hooks to manage navigation
   const router = useRouter();
+  const { userGoal, userCalorieInfo, loading } = useUserInformationContext();
 
   return (
     // The calorie container
@@ -22,12 +21,14 @@ const Calories = () => {
         {/** Title of the container */}
         <h1 className="font-bold text-xl">Calories</h1>
         {/** If the user has no objective */}
-        <button
-          className="w-fit bg-black text-white px-3 py-2 rounded-2xl text-center hover:opacity-75 hover:cursor-pointer"
-          onClick={() => router.push("?modal=food", { scroll: false })}
-        >
-          Add food +
-        </button>
+        {userGoal.calorie !== 0 && (
+          <button
+            className="w-fit bg-black text-white px-3 py-2 rounded-2xl text-center hover:opacity-75 hover:cursor-pointer"
+            onClick={() => router.push("?modal=food", { scroll: false })}
+          >
+            Add food +
+          </button>
+        )}
       </div>
       <div className="bg-white p-10 rounded-3xl border border-neutral-400">
         <DisplayCalories />
