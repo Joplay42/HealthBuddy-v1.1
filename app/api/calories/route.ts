9621bce53707c1276632b4f3f6b4d5eaedd8ca5b
Joplay42 <jojobanjo2611@gmail.com>
2025-04-01 20:16:53 -0400

    Calories POST backend implemented

--- app/api/calories/route.ts
+++ app/api/calories/route.ts
@@ -1,5 +1,5 @@
 import { db } from "@/config/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { NextResponse } from "next/server";
 
 export const GET = async (request: Request) => {
@@ -38,6 +38,66 @@ export const GET = async (request: Request) => {
     return new NextResponse(
       JSON.stringify({
         message: "Document has been found",
+        data: data.data(),
+      }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    // Return new error response
+    return new NextResponse(
+      JSON.stringify({
+        message: "An error occured while trying to fetch the users objective",
+        error: error.message,
+      }),
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = async (request: Request) => {
+  // Error handling
+  try {
+    // The new searchParams
+    const { searchParams } = new URL(request.url);
+    // Get the userid
+    const userId = searchParams.get("userid");
+
+    // handle null values
+    if (!userId) {
+      return new NextResponse(
+        JSON.stringify({
+          message: "Missing parameters",
+        }),
+        { status: 422 }
+      );
+    }
+
+    // Create UserCalorie Firestore doc
+    const fireStoreUserCalorieDoc = await doc(db, "UserCalorieData", userId);
+    // Blank UserCalorie Firestore doc
+    await setDoc(
+      fireStoreUserCalorieDoc,
+      { calorie: 0, carbs: 0, fat: 0, protein: 0 },
+      { merge: true }
+    );
+
+    // Get the doc
+    const data = await getDoc(fireStoreUserCalorieDoc);
+
+    // Handle null values
+    if (!data.exists()) {
+      return new NextResponse(
+        JSON.stringify({
+          message: `No user has been found with an id of ${userId}`,
+        }),
+        { status: 404 }
+      );
+    }
+
+    // Return the doc
+    return new NextResponse(
+      JSON.stringify({
+        message: "Document has been created",
         data: data.data(),
       }),
       { status: 200 }
