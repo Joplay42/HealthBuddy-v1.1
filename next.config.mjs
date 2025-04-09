63256a3f5639ec574ba31391c5cb1543d2a8efb1
Joplay42 <jojobanjo2611@gmail.com>
2025-04-09 11:55:16 -0400

    Fixed the deployement error for the useSearchParams

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false, // Disable the warning
+  },
+};
 
 export default nextConfig;
